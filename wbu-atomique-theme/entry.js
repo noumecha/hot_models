25b03b907c2b2d76495e33f75103af6458e1bd30
noumecha <noumechaivan@gmail.com>
2022-06-15 03:23:50 +0100

    maj : tmc

--- wbu-atomique-theme/entry.js
+++ wbu-atomique-theme/entry.js
@@ -1,8 +1,3 @@
 module.exports = {
     hot_models_overview_section: "./src/js/sections/hot_models_overview_section.js"
 };
-
-// export default {
-//   "formatage-models-footer1":
-//     "./src/js/sections/footers/formatage-models-footer1.js",
-// };
