598db952b0fc5121dfd419b5e12d6e2e53fe2381
noumecha <noumechaivan@gmail.com>
2022-06-23 22:28:56 +0100

    maj : tmc

--- src/Plugin/Layout/Sections/Menus/HotModelsHotlockMenu.php
+++ src/Plugin/Layout/Sections/Menus/HotModelsHotlockMenu.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\hot_models\Plugin\Layout\Sections;
+namespace Drupal\hot_models\Plugin\Layout\Sections\Menus;
 
 use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
 use Drupal\formatage_models\FormatageModelsThemes;
