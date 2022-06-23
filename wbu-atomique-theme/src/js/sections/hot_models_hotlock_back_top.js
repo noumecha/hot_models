6bcfd1bafd1448538e9ed4ea863bacd837a88647
noumecha <noumechaivan@gmail.com>
2022-06-23 02:21:04 +0100

    maj : tmc

--- wbu-atomique-theme/src/js/sections/hot_models_hotlock_back_top.js
+++ wbu-atomique-theme/src/js/sections/hot_models_hotlock_back_top.js
@@ -1,1 +1,2 @@
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/hotlock/htl-bac-top.js";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/hotlock/htl-bac-top.scss";
