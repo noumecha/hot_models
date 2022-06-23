58d6528f13eb7bee20ff38230e5627140fab02fa
noumecha <noumechaivan@gmail.com>
2022-06-23 12:57:12 +0100

    maj : tmc

--- wbu-atomique-theme/src/js/sections/menus/hot_models_hotlock_menu.js
+++ wbu-atomique-theme/src/js/sections/menus/hot_models_hotlock_menu.js
@@ -1,1 +1,3 @@
-import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/hotlock/header.scss";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/hotlock/hot-header.scss";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/header.scss";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/js/header.js";
