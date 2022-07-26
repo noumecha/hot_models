6fc55d2c2563ea3f5e8257ddc6efebca5835d42f
noumecha <noumechaivan@gmail.com>
2022-07-26 10:28:48 +0100

    maj : tmc

--- src/Plugin/Layout/Sections/HotModelsHongoCollectionSection.php
+++ src/Plugin/Layout/Sections/HotModelsHongoCollectionSection.php
@@ -47,7 +47,7 @@ class HotModelsHongoCollectionSection extends FormatageModelsSection
     public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
         // TODO Auto-generated method stub
         parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
-        $this->pluginDefinition->set('icon', drupal_get_path('module', 'hot_models') . "/icones/sections/hot_models_hotlock_hero.png");
+        $this->pluginDefinition->set('icon', drupal_get_path('module', 'hot_models') . "/icones/sections/hot_models_hongo_collection_section.png");
     }
   
     /**
